7ed2353f8b18ca9ef5b8597b7666da5a36ce3dae
ntutar <nazmitutar@gmail.com>
2020-11-01 21:06:30 +1000

    added new codes

---
+++ weenHallo/src/main/java/ween.java
@@ -0,0 +1,5 @@
+public class ween {
+    public static void main(String[] args) {
+        System.out.println("hallo");
+    }
+}
---
+++ weenHallo/src/test/java/HalloTest.java
@@ -0,0 +1,6 @@
+public class HalloTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println();
+    }
+}
