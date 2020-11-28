844ffd1797199b94a81f2129d713273c1bef2795
ntutar <nazmitutar@gmail.com>
2020-11-29 07:59:27 +1000

    new day4 DB connection created

---
+++ JDBC_Project_B20/src/test/java/day4/MySQL_Test.java
@@ -0,0 +1,38 @@
+package day4;
+
+import utility.ConfigurationReader;
+import utility.DB_Utility;
+
+public class MySQL_Test {
+
+    public static void main(String[] args) {
+
+//        String connectionStr = ConfigurationReader.getProperty("library1.database.url");
+//        String username = ConfigurationReader.getProperty("library1.database.username");
+//        String password = ConfigurationReader.getProperty("library1.database.password");
+//
+//        DB_Utility.createConnection(connectionStr,username,password);
+//
+//        DB_Utility.runQuery("SELECT * FROM books");
+//        DB_Utility.displayAllData();
+
+        String connectionStr = ConfigurationReader.getProperty("library2.database.url");
+        String username = ConfigurationReader.getProperty("library2.database.username");
+        String password = ConfigurationReader.getProperty("library2.database.password");
+
+        DB_Utility.createConnection(connectionStr,username,password);
+
+        DB_Utility.runQuery("SELECT * FROM book_borrow");
+        DB_Utility.displayAllData();
+
+
+
+
+
+        DB_Utility.destroy();
+
+
+
+    }
+
+}
