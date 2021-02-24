6ea1a5c23b88e1077e5d1a75f19f63d3e46af50d
ntutar <nazmitutar@gmail.com>
2021-02-25 09:39:51 +1000

    added new lines

--- Halloween2020/src/test/java/HalloweenTest.java
+++ Halloween2020/src/test/java/HalloweenTest.java
@@ -1,5 +1,10 @@
 public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
