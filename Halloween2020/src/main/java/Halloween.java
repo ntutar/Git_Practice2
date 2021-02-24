1db55eb8f7b794fa83f104fe155b3302758ea5cb
ntutar <nazmitutar@gmail.com>
2021-02-25 09:38:31 +1000

    added new lines

--- Halloween2020/src/main/java/Halloween.java
+++ Halloween2020/src/main/java/Halloween.java
@@ -1,5 +1,9 @@
 public class Halloween {
     public static void main(String[] args) {
         System.out.println("second class");
+        System.out.println("second class");
+        System.out.println("second class");
+        System.out.println("second class");
+        System.out.println("second class");
     }
 }
